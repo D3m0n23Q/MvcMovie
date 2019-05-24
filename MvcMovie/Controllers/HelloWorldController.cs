a6840179e0d1649f57de3a667c5fe39d7212ba41
Damian Evans <z3464512@student.unsw.edu.au>
2019-05-24 22:21:56 +1000

    Added a new HelloWorld Controller.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Action(string name, int id)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, num is {id}");
+        }
+    }
+}
