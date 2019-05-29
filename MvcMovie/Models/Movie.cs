8e3880e94fb3ceacf1a02e6b69a0e3ff38f83352
Damian Evans <z3464512@student.unsw.edu.au>
2019-05-29 14:14:47 +1000

    Fixed an issue with the SQL generated by EF Core referencing the wrong database table.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
+    [Table("Movie")]
     public class Movie
     {
         public int Id { get; set; }
